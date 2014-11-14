7680c7031d4241a796ad6dd1de8ef957d0138ef5
Jeremy Foster <jeremy.foster@live.com>
2014-11-13 22:30:41 -0800

    original commit

---
+++ app.js
@@ -0,0 +1,12 @@
+﻿var cylon = require('cylon');
+
+cylon.robot({
+    connection: { name: 'edison', adaptor: 'intel-iot' },
+    devices: [
+        { name: 'pin13', driver: 'led', pin: 13 },
+    ]
+}).on('ready', function (eddie) {
+    every((0.5).second(), function () {
+		eddie.pin13.toggle();
+    });
+}).start();
